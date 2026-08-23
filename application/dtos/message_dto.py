55ffe2cfe2e6eb24cc2bedd1f77c1f5bada5ecdb
Farnam Jahangard <farnamjahangard@gmail.com>
2026-08-23 20:02:54 +0330

    feat(application): add constructor to message dto

--- application/dtos/message_dto.py
+++ application/dtos/message_dto.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from domain.models.message import Message
+
 
 @dataclass
 class MessageDTO:
@@ -11,3 +13,15 @@ class MessageDTO:
     text: str
     word_count: int
     is_question: bool
+
+    @classmethod
+    def from_domain(cls, message: Message) -> "MessageDTO":
+        return cls(
+            id=message.id,
+            sender_id=message.sender_id,
+            sender_name=message.sender_name,
+            timestamp=message.timestamp,
+            text=message.text,
+            word_count=message.word_count,
+            is_question=message.is_question,
+        )
